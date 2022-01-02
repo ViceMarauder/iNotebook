3f85a7ed8ac3a5c01bb7e3ef4aa7002517aa2ec6
chirayupant <chirayupant@yahoo.com>
2022-01-02 19:29:59 +0530

    Working in authentication

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
 # iNotebook
 iNotebook is your notebook on the cloud. *It is a MERN app*. It uses Express as its **backend**.This project is inspired by [GithubRepo](https://github.com/CodeWithHarry/iNotebook-React).
 
-## Continue from [Here](https://www.youtube.com/watch?v=N4GHLaCSXUg&list=PLu0W_9lII9agx66oZnT6IyhcMIbUMNMdt&index=46)
+## Continue from [Here](https://www.youtube.com/watch?v=inEhzp3nD0M&list=PLu0W_9lII9agx66oZnT6IyhcMIbUMNMdt&index=48)
--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,15 +1,37 @@
 const express = require('express')
 const User = require('../models/User')
 const router = express.Router()
+const { body, validationResult } = require('express-validator');
 
-//Create a User using:POST "/api/auth".Doesn't require auth
 
-
-router.post('/', (req,res)=> {
-    console.log(req.body)
-    const user = User(req.body)
-    user.save()
-    res.send("hello")
+//Create a User using:POST "/api/auth/createuser".Doesn't require auth
+router.post('/createuser', [
+    body('name','Enter a valid name').isLength({ min: 3 }),
+    body('email','Enter a valid email').isEmail(),
+    body('password','Password must be atleast 5 characters').isLength({ min: 5 })
+],async (req,res)=> {
+    //If there are errors,return bad request and the errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() })
+    }
+    //Check whether the user with this email exists already
+    try{
+    let user = await User.findOne({email:req.body.email})
+    if(user){
+        return res.status(400).json({error: "Sorry a user with this email already exist"})
+    }
+    user = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password
+      })
+    res.json(user)
+    }
+    catch(error){
+        console.error(error.message)
+        res.status(500).send("Some error occured")
+    }
 })
 
 module.exports = router
